649056989eddf68e35fa42183cac13789a768c61
Joshua Lynch <jklynch@email.arizona.edu>
2018-06-28 15:49:34 -0400

    get test data from irods

--- stampede2/test.sh
+++ stampede2/test.sh
@@ -3,11 +3,13 @@
 #SBATCH -A iPlant-Collabs
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 00:30:00
+#SBATCH -t 00:10:00
 #SBATCH -p skx-normal
 #SBATCH -J imicrobe-prokka-test
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --mail-user jklynch@email.arizona.edu
+
+iget -f /iplant/home/jklynch/test/imicrobe-prokka/test.fa
 
 # include --force to overwrite output from previous tests
 ./run.sh test.fa --cpus 0 --force --outdir PROKKA_TEST
